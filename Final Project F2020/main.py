ccd62a6ca85b27826d268c63c28d58f5addc74e2
senaitdafa <sdafa@umich.edu>
2020-12-13 16:59:04 -0500

    Update main.py

--- Final Project F2020/main.py
+++ Final Project F2020/main.py
@@ -8,8 +8,14 @@ def main():
     cur, conn = create_database(DATABASE)
     create_restaurant_table(cur, conn)
     create_customer_table(cur, conn)
-
-    restaurants = ("sava's", ) #we need 20 to get 100 customers
+    #"sava's"
+    restaurants = ("Seva Restaurant", ) #we need 20 to get 100 customers
+    rests = [("Seva Restaurant",),("Shake Shack",),("Slows Bar BQ"),("Stella's Lounge",),
+("Sundance Grill & Bar",),("Supino Pizzeria",),("Taco King",),("Taqueria Mi Pueblo",),("Terra",),
+("Texas de Brazil",),("The Apparatus Room",),("The Chop House",),
+("The Earle Restaurant",),("The Green Well",),("The Kitchen By Wolfgang Puck",),("The Lunch Room",),
+("The Old Goat",),("The Pit Stop",),("The West End Grill",),("The Winchester",),("Tomukun Korean Barbeque",),
+("Vicente's Cuban Cuisine",),("Vinology Restaurant & Event Space",),("Weber's Restaurant",)]
 
     customer_count = 0
     for i, restaurant in enumerate(restaurants, 1):
